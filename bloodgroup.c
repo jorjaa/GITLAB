692f21f598f7b39f52ab2c39ce78b2f99f577de0
jorjaa <jaa_sorawit@hotmail.co.uk>
2018-11-26 21:10:17 +0700

    change blood.c

--- bloodgroup.c
+++ bloodgroup.c
@@ -9,7 +9,7 @@ void main()
     scanf("%s", Name);
     do
     {
-      printf("What is your blood group [A, B, AB, or O]? ");
+      printf("What is your blood group [B, A, O, or AB]? ");
       scanf("%s", G);
 
       if (strcmp(G, "A") !=0  &&
@@ -44,5 +44,5 @@ void main()
     printf("\nContinue (YES for Yes)? ");
     scanf("%s", answer);
   } while (strcmp(answer, "YES") == 0);
-  printf("Goodbye\n");
+  printf("Bye bye\n");
 }
